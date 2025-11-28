b89f3e738e25dfc6ff695d4670921186e8e4aeb6
Seedswirl <angelique.carpo@gmail.com>
2025-11-28 19:11:22 +0200

    feat: add Hello World

---
+++ src/gr/aueb/sev/helloapp/HelloWorld.java
@@ -0,0 +1,12 @@
+package gr.aueb.sev.helloapp;
+/*
+A show-how-to-create-a-git-project
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+    }
+}
+
