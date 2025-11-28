0e2c847bee730c940e1e9c9633f9cbf1109d311a
Seedswirl <angelique.carpo@gmail.com>
2025-11-28 20:03:18 +0200

    feat: add subtraction functionality

---
+++ src/gr/aueb/sev/helloapp/SubApp.java
@@ -0,0 +1,11 @@
+package gr.aueb.sev.helloapp;
+
+public class SubApp {
+    public static void main(String[] args) {
+        System.out.println(sub (10,5));
+    }
+
+    public static int sub(int a, int b){
+        return a - b;
+    }
+}
